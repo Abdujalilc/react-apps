783fcb0bc622038179560cb3a45a0140feaed470
abdujalilc <abdujalilc@outlook.com>
2023-07-11 20:57:23 +0500

    notes are added

--- redux-essentials-counter/src/app/store.js
+++ redux-essentials-counter/src/app/store.js
@@ -3,6 +3,7 @@ import counterReducer from '../features/counter/counterSlice';
 
 export default configureStore({
   reducer: {
+    //counterReducer is return of createSlice() method
     counter: counterReducer,
   },
 });
