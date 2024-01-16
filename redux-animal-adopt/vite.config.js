5c2a7ec6e08018da0b59c6a7f1877ccc9cbccad5
abdujalilc <abdujalilc@outlook.com>
2024-01-16 15:27:28 +0500

    cleanings

--- redux-animal-adopt/vite.config.js
+++ redux-animal-adopt/vite.config.js
@@ -2,6 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  server: {
+    host: '127.0.0.1',
+    port: 3000,
+    //open: true
+  },
   plugins: [react()],
   root: "src",
+  optimizeDeps: {
+    debugger: 'chrome',
+  },
 });
