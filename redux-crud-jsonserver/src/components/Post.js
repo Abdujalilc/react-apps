e29d2aadf008357927f0c0547f5fae333af35446
abdujalilc <abdujalilc@outlook.com>
2024-02-12 19:05:46 +0500

    minor fixes

--- redux-crud-jsonserver/src/components/Post.js
+++ redux-crud-jsonserver/src/components/Post.js
@@ -1,10 +1,11 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Like from "./Like";
-import {useDispatch, useSelector} from "react-redux";
-import {isEmpty} from "./Utils";
-import {deletePost, editPost} from "../actions/post.action";
+import { useDispatch, useSelector } from "react-redux";
+import { isEmpty } from "./Utils";
+import { deletePost, editPost } from "../actions/post.action";
+import myImage from "../assets/253-1500x400.jpg";
 
-const Post = ({post}) => {
+const Post = ({ post }) => {
   const [editToggle, setEditToggle] = useState(false);
   const user = useSelector((state) => state.userReducer)
   const [editContent, setEditContent] = useState(post.content)
@@ -26,44 +27,44 @@ const Post = ({post}) => {
   }
 
   return (
-      <div className="post">
-        {!isEmpty(user) && user.pseudo === post.author && (
-            <div className="edit-delete">
-              <img
-                  src="./icons/edit.svg"
-                  alt="edit"
-                  onClick={() => setEditToggle(!editToggle)}
-              />
-              <img
-                  src="./icons/delete.svg"
-                  alt="delete"
-                  onClick={() => dispatch(deletePost(post.id))}
-              />
-            </div>
-        )}
-
-        <h2>{post.title}</h2>
-        <img
-            src="https://picsum.photos/1500/400"
-            className="post-img"
-            alt="img-post"
-        />
-
-        {editToggle ? (
-            <form onSubmit={e => handleEdit(e)}>
-              <textarea autoFocus={true} defaultValue={post.content}
-                        onChange={e => setEditContent(e.target.value)}></textarea>
-              <input type="submit" value="Validate modification"/>
-            </form>
-        ) : (
-            <p>{post.content}</p>
-        )}
-
-        <div className="author">
-          <h5>{post.author}</h5>
-          <Like post={post}/>
+    <div className="post">
+      {!isEmpty(user) && user.pseudo === post.author && (
+        <div className="edit-delete">
+          <img
+            src="./icons/edit.svg"
+            alt="edit"
+            onClick={() => setEditToggle(!editToggle)}
+          />
+          <img
+            src="./icons/delete.svg"
+            alt="delete"
+            onClick={() => dispatch(deletePost(post.id))}
+          />
         </div>
+      )}
+
+      <h2>{post.title}</h2>
+      <img
+        src={myImage}
+        className="post-img"
+        alt="img-post"
+      />
+
+      {editToggle ? (
+        <form onSubmit={e => handleEdit(e)}>
+          <textarea autoFocus={true} defaultValue={post.content}
+            onChange={e => setEditContent(e.target.value)}></textarea>
+          <input type="submit" value="Validate modification" />
+        </form>
+      ) : (
+        <p>{post.content}</p>
+      )}
+
+      <div className="author">
+        <h5>{post.author}</h5>
+        <Like post={post} />
       </div>
+    </div>
   );
 };
 
