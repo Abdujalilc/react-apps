8efcf5a05deec91a8074b39c8d74b776f602b5d5
abdujalilc <abdujalilc@outlook.com>
2025-04-01 13:01:12 +0500

    just changes

--- react-intro-projects/05-useeffect/src/SearchParams.jsx
+++ react-intro-projects/05-useeffect/src/SearchParams.jsx
@@ -9,83 +9,66 @@ const SearchParams = () => {
   const [breed, setBreed] = useState("");
   const breeds = [];
 
-  useEffect(() => {
-    requestPets();
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+  useEffect(() => { requestPets(); }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   async function requestPets() {
-    const res = await fetch(
-      `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
-    );
+    const res = await fetch(`http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`);
     const json = await res.json();
-
     setPets(json.pets);
   }
 
   return (
-    <div className="search-params">
-      <form>
-        <label htmlFor="location">
-          Location
-          <input
-            id="location"
-            value={location}
-            placeholder="Location"
-            onChange={(e) => setLocation(e.target.value)}
-          />
-        </label>
+    <div className="search-params" >
+      <div>
+        <form>
+          <label htmlFor="location">
+            Location
+            <input id="location" value={location} placeholder="Location" onChange={(e) => setLocation(e.target.value)} />
+          </label>
 
-        <label htmlFor="animal">
-          Animal
-          <select
-            id="animal"
-            value={animal}
-            onChange={(e) => {
-              setAnimal(e.target.value);
-              setBreed("");
-            }}
-            onBlur={(e) => {
-              setAnimal(e.target.value);
-              setBreed("");
-            }}
-          >
-            <option />
-            {ANIMALS.map((animal) => (
-              <option key={animal} value={animal}>
-                {animal}
-              </option>
-            ))}
-          </select>
-        </label>
+          <label htmlFor="animal">
+            Animal
+            <select id="animal" value={animal}
+              onChange={(e) => {
+                setAnimal(e.target.value);
+                setBreed("");
+              }}
+              onBlur={(e) => {
+                setAnimal(e.target.value);
+                setBreed("");
+              }}>
+              <option />
+              {ANIMALS.map((animal) => (
+                <option key={animal} value={animal}>
+                  {animal}
+                </option>
+              ))}
+            </select>
+          </label>
 
-        <label htmlFor="breed">
-          Breed
-          <select
-            disabled={!breeds.length}
-            id="breed"
-            value={breed}
-            onChange={(e) => setBreed(e.target.value)}
-            onBlur={(e) => setBreed(e.target.value)}
-          >
-            <option />
-            {breeds.map((breed) => (
-              <option key={breed} value={breed}>
-                {breed}
-              </option>
-            ))}
-          </select>
-        </label>
+          <label htmlFor="breed">
+            Breed
+            <select disabled={!breeds.length} id="breed" value={breed}
+              onChange={(e) => setBreed(e.target.value)}
+              onBlur={(e) => setBreed(e.target.value)}>
+              {breeds.map((breed) => (
+                <option key={breed} value={breed}>
+                  {breed}
+                </option>
+              ))}
+            </select>
+          </label>
 
-        <button>Submit</button>
-      </form>
-      {pets.map((pet) => (
-        <Pet
-          name={pet.name}
-          animal={pet.animal}
-          breed={pet.breed}
-          key={pet.id}
-        />
-      ))}
+          <button>Submit</button>
+        </form>
+      </div>
+
+      <div>
+        {pets.map((pet) => (
+          <Pet name={pet.name} animal={pet.animal} breed={pet.breed} key={pet.id} />
+        ))}
+      </div>
+
     </div>
   );
 };
